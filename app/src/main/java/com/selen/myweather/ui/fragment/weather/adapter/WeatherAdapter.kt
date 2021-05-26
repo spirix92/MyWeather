6064523e65231d0585f888341721cba2628d4b41
Aleksey Pyaterko <pyaterko92@mail.ru>
2021-05-26 20:13:04 +0300

    - добавил нестандартный элемент в список погоды

--- app/src/main/java/com/selen/myweather/ui/fragment/weather/adapter/WeatherAdapter.kt
+++ app/src/main/java/com/selen/myweather/ui/fragment/weather/adapter/WeatherAdapter.kt
@@ -14,27 +14,61 @@ import com.selen.myweather.helper.DateParser
 /**
  * @author Pyaterko Aleksey
  */
-class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() {
+class WeatherAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private var items = mutableListOf<WeatherList>()
     private var dateMapper = DateParser()
     private var tempUnit: String = ""
 
+    //    добавлять параметр типа View для элемента списка не стал,
+//    т.к. нестандартная view будет только на одном месте списка для демонстрации,
+//    а не в случайных местах
     fun setData(list: List<WeatherList>, tempUnit: String) {
         items = list.toMutableList()
         this.tempUnit = tempUnit
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = WeatherViewHolder(
-        LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_weather_fragment_weather, parent, false)
-    )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == VIEW_TYPE_ONE) {
+            WeatherViewHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_weather_fragment_weather, parent, false)
+            )
+        } else {
+            AdvertisingViewHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_weather_fragment_advertising, parent, false)
+            )
+        }
+    }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int {
+        return if (items.size > ADVERTISING_POSITION) {
+            items.size + 1
+        } else {
+            items.size
+        }
+    }
 
-    override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
-        holder.bind(items[position])
+    override fun getItemViewType(position: Int): Int {
+        return if (position == ADVERTISING_POSITION) {
+            VIEW_TYPE_TWO
+        } else {
+            VIEW_TYPE_ONE
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (position == ADVERTISING_POSITION) {
+            (holder as AdvertisingViewHolder).bind()
+        } else {
+            if (position < ADVERTISING_POSITION) {
+                (holder as WeatherViewHolder).bind(items[position])
+            } else {
+                (holder as WeatherViewHolder).bind(items[position - 1])
+            }
+        }
     }
 
     inner class WeatherViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -79,7 +113,22 @@ class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() 
 
 //            идентификатор иконки
             item.weather.first().icon
-
         }
     }
+
+    inner class AdvertisingViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private var advertising =
+            view.findViewById<TextView>(R.id.item_weather_fragment_advertising_text_view_text)
+
+        fun bind() {
+            advertising.text = advertising.context.getString(R.string.advertising_text)
+        }
+    }
+
+    companion object {
+        const val VIEW_TYPE_ONE = 1
+        const val VIEW_TYPE_TWO = 2
+        const val ADVERTISING_POSITION = 6
+    }
+
 }
